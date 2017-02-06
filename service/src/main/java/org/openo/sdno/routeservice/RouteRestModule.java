cddd60890c3ccb6ba5731a13f3d08be2381a84b2
chuwei <chuwei@huawei.com>
2017-02-06 10:21:56 +0800

    Coding for route
    Coding for route atomic service.
    
    SDNO-1102
    
    Change-Id: Iacef352723344ca64463e51bb99f315af826f006
    Signed-off-by: chuwei <chuwei@huawei.com>

---
+++ service/src/main/java/org/openo/sdno/routeservice/RouteRestModule.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openo.sdno.routeservice;
+
+import org.openo.sdno.framework.container.service.IRoaModule;
+import org.openo.sdno.overlayvpn.inventory.sdk.DbOwerInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * RouteService Rest Module Class. <br>
+ * 
+ * @author
+ * @version SDNO 0.5 Jan 9, 2017
+ */
+public class RouteRestModule extends IRoaModule {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RouteRestModule.class);
+
+    @Override
+    protected void destroy() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    protected void init() {
+        LOGGER.info("=====start route svc roa module=====");
+
+        DbOwerInfo.init("routeSvc", "routedb");
+    }
+}

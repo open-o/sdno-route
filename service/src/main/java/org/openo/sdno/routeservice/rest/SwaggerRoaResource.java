2eb3883455b04d295964526ebaeb4fc089305100
angilecoder <zhaoyangzhao.zhaoyang@huawei.com>
2017-03-23 11:36:27 +0800

    Refresh Route Swagger API document
    Refresh Route Swagger API document, use maven plugin to generate swagger file and deploy to service directory.
    
    SDNO-1476
    
    Signed-off-by: angilecoder <zhaoyangzhao.zhaoyang@huawei.com>
    Change-Id: I172502a903fb12be8681d8c5d80cd2aebb0f7424

---
+++ service/src/main/java/org/openo/sdno/routeservice/rest/SwaggerRoaResource.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openo.sdno.routeservice.rest;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.io.IOUtils;
+import org.openo.baseservice.remoteservice.exception.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * Swagger API Doc.<br/>
+ *
+ * @author
+ * @version SDNO 0.5 Mar 23, 2017
+ */
+@Service
+@Path("/sdnoroute/v1")
+public class SwaggerRoaResource {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SwaggerRoaResource.class);
+
+    private String swaggerFilePath;
+
+    public void setSwaggerFilePath(String swaggerFilePath) {
+        this.swaggerFilePath = swaggerFilePath;
+    }
+
+    /**
+     * Query api document.<br>
+     * 
+     * @return api document
+     * @throws ServiceException when read document failed
+     * @since SDNO 0.5
+     */
+    @GET
+    @Path("/swagger.json")
+    @Produces({MediaType.APPLICATION_JSON})
+    public String apidoc() throws ServiceException {
+        try (FileInputStream finStream = new FileInputStream(swaggerFilePath)) {
+            return IOUtils.toString(finStream);
+        } catch(IOException e) {
+            LOGGER.error("Read swagger json occurs exception");
+            throw new ServiceException("Read swagger json occurs exception");
+        }
+    }
+}
